ec8bf2dd9a7f0044003baa2954b0029747a94fa3
mikael <ragnarokz440@gmail.com>
2025-01-05 12:34:20 -0300

    Line number config

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("conform").formatters_by_ft = {
+
+  html = { "prettier" }, -- Specify "prettier" as the formatter for HTML files
+}
